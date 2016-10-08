9308e899ca8ddcd20d6b4539b9283fe6d3547742
cjq <c_gcsh@163.com>
2016-10-08 14:46:40 +0800

    PDO

---
+++ PDO.php
@@ -0,0 +1,16 @@
+<?php
+namespace db;
+class PDO implements IDatabase{
+    function connect()
+    {
+        // TODO: Implement connect() method.
+    }
+    function query()
+    {
+        echo "query";
+    }
+    function close()
+    {
+        // TODO: Implement close() method.
+    }
+}
