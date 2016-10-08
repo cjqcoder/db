c96b96201fa9197f28b83e5249d82cc4bc091dc5
cjq <c_gcsh@163.com>
2016-10-08 14:31:00 +0800

    PDO

---
+++ MYSQLI.php
@@ -0,0 +1,15 @@
+<?php
+namespace db;
+class MYSQLI implements IDatabase{
+    function connect()
+    {
+        // TODO: Implement connect() method.
+    }
+    function query()
+    {
+        // TODO: Implement query() method.
+    }
+    function close(){
+        // TODO: Implement close() method.
+    }
+}
